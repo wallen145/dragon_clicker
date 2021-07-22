e5fa4259564735bfc4bcfbd09cc34665b33ff57a
Jeff Allen <william_j_allen@outlook.com>
2021-07-22 18:08:20 -0400

    Javascript cleanup and add two more dragons

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ var dragons = [
     type: 'Fire',
     clicks: 0
   },
+  {
+    id: 4,
+    type: 'Water',
+    clicks: 0
+  },
+  {
+    id: 5,
+    type: 'Forest',
+    clicks: 0
+  },
 ];
 
 var backgrounds = ["#BFFCC6", "#DBDFFD6", "#F3FFE3", "#E7FFAC", "#FFFFD1", "#FFC9DE", "FFABAB", "#FFBEBC", "#FFCBC1", "#FFF5BA"];
@@ -24,16 +34,11 @@ function drawDragon(id) {
   console.log(`drawing dragon ${id}`);
   let collection = document.querySelector("#button-collection")
   let btnContainer = document.createElement("div");
-  btnContainer.className="button-container";
-  //btnContainer.style.backgroundColor = backgrounds[(Math.floor(Math.random() * backgrounds.length))]
-
-  //let typeDiv = document.createElement("div");
-  //typeDiv.innerHTML = `<font size="200%">${dragons[id].type}</font>`;
-  console.log(dragons[id].type, backgrounds[(Math.random() * backgrounds.length)]);
+  btnContainer.className = "button-container";
 
   let btn = document.createElement("button");
   btn.id = id;
-  btn.style.backgroundColor = backgrounds[(id%9)];
+  btn.style.backgroundColor = backgrounds[(id % 9)];
   //btn.appendChild(typeDiv);
   btn.innerHTML = '<font size="200%">&#128009;</font>'
 
@@ -43,21 +48,21 @@ function drawDragon(id) {
   btn.appendChild(div);
 
   let output = document.createElement("output");
-  output.className="click-output";
+  output.className = "click-output";
   output.id = "click-me-count";
   output.innerHTML = "0";
   btn.appendChild(output);
 
   btn.appendChild(div);
-  btn.addEventListener('click', function(e) {
+  btn.addEventListener('click', function (e) {
     dragons[id].clicks += 1;
     output.innerHTML = dragons[id].clicks;
- });
- btnContainer.innerHTML=`<h2>${dragons[id].type}</h2>`;
- btnContainer.appendChild(btn);
- collection.appendChild(btnContainer);
+  });
+  btnContainer.innerHTML = `<h2>${dragons[id].type}</h2>`;
+  btnContainer.appendChild(btn);
+  collection.appendChild(btnContainer);
 }
 
 for (var i = 0; i < dragons.length; i++) {
-  drawDragon(i); 
+  drawDragon(i);
 }
