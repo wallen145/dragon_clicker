0f42c889870772fee2d60849f6c7d40a59f93732
Jeff Allen <william_j_allen@outlook.com>
2021-07-29 16:21:02 -0400

    Menu dragon selection, dynamic display using innerHTML.

--- index.js
+++ index.js
@@ -1,109 +1,92 @@
 var dragon_img = "./images/dragon-154565_1280.png"
-var clicks = 0;
 var click_target_id;
-var dragons = [
-  {
-    id: 1,
-    type: 'Earth',
-    class_name: 'earthDragon',
-    clicks: 0
-  },
-  {
-    id: 2,
-    type: 'Wind',
-    class_name: 'windDragon',
-    clicks: 0
-  },
-  {
-    id: 3,
-    type: 'Fire',
-    class_name: 'fireDragon',
-    clicks: 0
-  },
-  {
-    id: 4,
-    type: 'Water',
-    class_name: 'waterDragon',
-    clicks: 0
-  },
-  {
-    id: 5,
-    type: 'Forest',
-    class_name: 'forestDragon',
-    clicks: 0
-  },
+var dragons = [{
+        id: 1,
+        type: 'Earth',
+        class_name: 'earthDragon',
+        clicks: 0,
+        background: "#BFFCC6"
+    },
+    {
+        id: 2,
+        type: 'Wind',
+        class_name: 'windDragon',
+        clicks: 0,
+        background: "#CBDFFD"
+    },
+    {
+        id: 3,
+        type: 'Fire',
+        class_name: 'fireDragon',
+        clicks: 0,
+        background: "#F3FFE3"
+    },
+    {
+        id: 4,
+        type: 'Water',
+        class_name: 'waterDragon',
+        clicks: 0,
+        background: "#E7FFAC"
+    },
+    {
+        id: 5,
+        type: 'Forest',
+        class_name: 'forestDragon',
+        clicks: 0,
+        background: "#FFFFD1"
+    },
 ];
-
-var backgrounds = ["#BFFCC6", "#DBDFFD6", "#F3FFE3", "#E7FFAC", "#FFFFD1", "#FFC9DE", "FFABAB", "#FFBEBC", "#FFCBC1", "#FFF5BA"];
 
 var navBar = document.getElementById("navBar");
 var visibleDragon;
 
 function drawMenuItem(id) {
-  console.log(dragons[id].type);
-  let unorderedList = document.createElement("ul");
-  let listItem = document.createElement("li");
-  listItem.setAttribute('id', dragons[id].type);
-  listItem.setAttribute('value', dragons[id].type);
-  //listItem.innerHTML = `<h2>${dragons[id].type;}</h2>`;
-  listItem.innerHTML = '<font size="18px">' + dragons[id].type; + '</font>';
-  listItem.addEventListener('click', function (e) {
-    if (typeof visibleDragon !== 'undefined') {
-      removeDragon(visibleDragon);
-    }
-    visibleDragon = id;
-    drawDragon(id);
-  });
-  unorderedList.appendChild(listItem);
-  navBar.appendChild(unorderedList);
+    console.log(dragons[id].type);
+    let unorderedList = document.createElement("ul");
+    let listItem = document.createElement("li");
+    listItem.setAttribute('id', dragons[id].type);
+    listItem.setAttribute('value', dragons[id].type);
+    listItem.innerHTML = '<font size="18px">' + dragons[id].type; + '</font>';
+    listItem.addEventListener('click', function(e) {
+        dragonDisplay(id);
+    });
+    unorderedList.appendChild(listItem);
+    navBar.appendChild(unorderedList);
 }
 
 for (var i = 0; i < dragons.length; i++) {
-  drawMenuItem(i);
+    drawMenuItem(i);
 }
 
-function removeDragon(id) {
-  let collection = document.querySelector("#button-collection");
-  let btnContainer = document.getElementById(`${id}`);
-  console.log("about to remove: ", id,  dragons[id].type);
-  btnContainer.remove();
-}
-
-function drawDragon(id) {
-  console.log(`drawing dragon ${id}`);
-  let collection = document.querySelector("#button-collection")
-  let btnContainer = document.createElement("div");
-  btnContainer.setAttribute("id", id);
-  btnContainer.className = "button-container";
-
-  let btn = document.createElement("button");
-  btn.id = id;
-  btn.style.backgroundColor = backgrounds[(id % 9)];
-
-  let img = document.createElement("img");
-  img.type = "image";
-  img.src = dragon_img;
-  img.className = `${dragons[id].class_name}`;
-
-  btn.appendChild(img);
-
-  let div = document.createElement("div");
-  div.className = "centered-text";
-  div.innerHTML = "Click Me!!!";
-  btn.appendChild(div);
-
-  let output = document.createElement("output");
-  output.className = "click-output";
-  output.id = "click-me-count";
-  output.innerHTML = dragons[id].clicks;
-  btn.appendChild(output);
-
-  btn.appendChild(div);
-  btn.addEventListener('click', function (e) {
+function incrementCount(id) {
     dragons[id].clicks += 1;
-    output.innerHTML = dragons[id].clicks;
-  });
-  btnContainer.innerHTML = `<h2>${dragons[id].type}</h2>`;
-  btnContainer.appendChild(btn);
-  collection.appendChild(btnContainer);
+    let output = document.querySelector("#click_count");
+    console.log(dragons[id].clicks);
+    output.innerHTML = `Click count: ${dragons[id].clicks}`;
+}
+
+function dragonFactory(id) {
+    console.log(dragons[id].type);
+    let dragonHTML = `
+    <div id="button_collection" style="background-color: ${dragons[id].background}">
+      <div id="button_container">
+        <output class="centered_text">${dragons[id].type}</output>
+        <button id="click_me" onClick="incrementCount(${id})">
+          <img src="${dragon_img}" class="${dragons[id].class_name}" alt="Dragon image"/>
+        </button>
+        <output id="click_count" class="centered_text">Click count: ${dragons[id].clicks}</output>
+      </div>
+    </div>  
+  `
+    return (dragonHTML);
+}
+
+function dragonDisplay(id) {
+    dragons[id].count = 0;
+    let attach_here = document.querySelector("#attach_here");
+    if (typeof collection !== 'undefined') {
+        collection.innerHTML = "";
+    }   
+    let dragon = dragonFactory(id)  
+    attach_here.innerHTML = dragon; 
 }
